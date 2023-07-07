6668af885791e1a9bcc6d4d63b6ce4daf94141e8
otmorales23 <oliverwills0227@gmail.com>
2023-07-06 21:38:12 -0700

    some cool stuff happened i swear

--- public/js/login.js
+++ public/js/login.js
@@ -37,7 +37,7 @@ const signupFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace('/contacts');
+      document.location.replace('/profile');
     } else {
       alert(response.statusText);
     }
